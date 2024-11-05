ca7532bfa9b342166e617ae0ac1d928384893b37
shana-ck <s.klein1127@gmail.com>
2024-11-05 17:08:31 -0500

    surprise kornacki!

--- index.js
+++ index.js
@@ -1,49 +1,51 @@
 const root = document.documentElement;
-const prediction = document.getElementById('prediction');
-let curAngle = getComputedStyle(root).getPropertyValue('--angle');
+const prediction = document.getElementById("prediction");
+let curAngle = getComputedStyle(root).getPropertyValue("--angle");
 let angleVal = parseInt(curAngle.slice(0, -3));
-const modal = document.getElementById('modal');
-const modalClose = document.getElementById('close-modal');
-const kornackiText = document.getElementById('kornacki-text');
-const reset = document.getElementById('reset');
+const modal = document.getElementById("modal");
+const modalClose = document.getElementById("close-modal");
+const kornackiText = document.getElementById("kornacki-text");
+const reset = document.getElementById("reset");
 let count = 0;
+let surprise = 9;
 
 function closeModal() {
-  modal.classList.remove('show-modal');
+  modal.classList.remove("show-modal");
+  surprise = Math.floor(Math.random() * 15) + 5;
 }
 
 function setPrediction(angleVal) {
   if (angleVal <= 45) {
-    prediction.textContent = 'Strong Trump';
+    prediction.textContent = "Strong Trump";
   } else if (angleVal > 45 && angleVal < 90) {
-    prediction.textContent = 'Lean Trump';
+    prediction.textContent = "Lean Trump";
   } else if (angleVal === 90) {
-    prediction.textContent = 'Toss-up';
+    prediction.textContent = "Toss-up";
   } else if (angleVal > 90 && angleVal < 135) {
-    prediction.textContent = 'Lean Harris';
+    prediction.textContent = "Lean Harris";
   } else if (angleVal >= 135) {
-    prediction.textContent = 'Strong Harris';
+    prediction.textContent = "Strong Harris";
   }
 }
 
 function poll() {
   angleVal = Math.floor(Math.random() * 181);
-  root.style.setProperty('--angle', angleVal + 'deg');
+  root.style.setProperty("--angle", angleVal + "deg");
   setPrediction(angleVal);
   count++;
-  if (count > 9) {
+  if (count > surprise) {
     kornacki();
     count = 0;
   }
 }
 
-modalClose.addEventListener('click', closeModal);
-reset.addEventListener('click', closeModal);
-window.addEventListener('click', e => {
+modalClose.addEventListener("click", closeModal);
+reset.addEventListener("click", closeModal);
+window.addEventListener("click", e => {
   e.target === modal ? closeModal() : false;
 });
 
 function kornacki() {
-  modal.classList.add('show-modal');
+  modal.classList.add("show-modal");
   kornackiText.textContent = `${prediction.textContent}`;
 }
