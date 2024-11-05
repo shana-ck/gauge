916399350df43af6c47938f1153a19085307b884
shana-ck <s.klein1127@gmail.com>
2024-11-05 15:43:10 -0500

    kornacki

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const root = document.documentElement;
 const prediction = document.getElementById('prediction');
 let curAngle = getComputedStyle(root).getPropertyValue('--angle');
 let angleVal = parseInt(curAngle.slice(0, -3));
+const modal = document.getElementById('modal');
+const modalClose = document.getElementById('close-modal');
+let count = 0;
+
+function showModal() {
+  modal.classList.add('show-modal');
+}
+
+function closeModal() {
+  modal.classList.remove('show-modal');
+}
 
 function setPrediction(angleVal) {
   if (angleVal <= 45) {
@@ -21,4 +32,16 @@ function poll() {
   angleVal = Math.floor(Math.random() * 181);
   root.style.setProperty('--angle', angleVal + 'deg');
   setPrediction(angleVal);
+  count++;
+  if (count > 10) {
+    kornacki();
+    count = 0;
+  }
 }
+
+modalClose.addEventListener('click', closeModal);
+window.addEventListener('click', e => {
+  e.target === modal ? closeModal() : false;
+});
+
+function kornacki() {}
